0aa9ee02839fe2575e629c906e7f336047af7e1e
Roy Quilor <royquilor@gmail.com>
2025-02-18 15:33:47 +0000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "./App.css";
+
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+function App() {
+  return (
+    <div className="[--header-height:calc(theme(spacing.14))]">
+      <SidebarProvider className="flex flex-col">
+        <SiteHeader />
+        <div className="flex flex-1">
+          <AppSidebar />
+          <SidebarInset>
+            {/* <SidebarTrigger /> */}
+            <div className="flex flex-1 flex-col gap-4 p-4">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+              </div>
+              <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+            </div>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/site-header.tsx
@@ -0,0 +1,9 @@
+export function SiteHeader() {
+  return (
+    <header className="flex sticky top-0 z-50 w-full items-center border-b bg-background">
+      <div className="flex h-[--header-height] w-full items-center gap-2 px-4">
+        Header
+      </div>
+    </header>
+  );
+}
